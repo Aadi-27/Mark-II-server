0f02d51f02afac6fac757268ce1acd95e0a95bb2
Aditya <rajaditya27145@gmail.com>
2020-11-21 10:56:48 +0530

    cleaning up

--- sw.js
+++
@@ -1,8 +0,0 @@
-// install service worker
-self.addEventListener("install", (e) => console.log("SW installed"));
-
-// activate service worker
-self.addEventListener("activate", (e) => console.log("SW active"));
-
-// fetch event
-self.addEventListener("fetch", (e) => console.log("fetch event", e));
